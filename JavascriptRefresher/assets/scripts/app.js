f2f8bffac674466b8ab96c2c30a4ade0b29e5d68
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-10-06 16:27:34 +0100

    import & export

---
+++ JavascriptRefresher/assets/scripts/app.js
@@ -0,0 +1,8 @@
+import { Name, pren } from "./utils.js";
+import * as info from "./utils.js"
+import prenom from "./utils.js"
+console.log(Name);
+console.log(pren);
+console.log(info.Name);
+console.log(info.pren);
+console.log(prenom);
---
+++ JavascriptRefresher/assets/scripts/utils.js
@@ -0,0 +1,4 @@
+export let Name = "MEBARKI";
+export let pren = "Med Amine";
+let Prenom = "Amine";
+export default Prenom;
