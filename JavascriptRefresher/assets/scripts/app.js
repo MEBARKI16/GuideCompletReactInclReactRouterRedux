82f5e3865d7c2df3162e1d4139f8b669abe01e81
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-10-08 16:49:50 +0100

    Revisiting Functions & Parameters

--- JavascriptRefresher/assets/scripts/app.js
+++ JavascriptRefresher/assets/scripts/app.js
@@ -14,3 +14,28 @@ console.log(nom);
 const adress = "ALGER";
 // a constant does not change, it is just read
 console.log(adress);
+// 3-Revisiting Functions & Parameters
+function showName(){
+    console.log("Amine MEBARKI");
+}
+showName();
+
+function showName(Name){
+    console.log(Name);
+}
+showName("Amine MEBARKI");
+
+function showName(nom , Prenom){
+    console.log(nom + Prenom);
+}
+showName("Amine"," MEBARKI");
+
+function showName(nom , Prenom = "MEBARKI"){
+    console.log(nom + Prenom);
+}
+showName("Amine");
+
+function showName(nom , Prenom = "MEBARKI"){
+    console.log(nom + Prenom);
+}
+showName("Amine"," IKRABEM");
