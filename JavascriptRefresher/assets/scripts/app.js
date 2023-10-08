b328610f78ec8f79e72f11a9304cde2decf5171d
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-10-08 16:09:04 +0100

    Revisiting Variables & Values

--- JavascriptRefresher/assets/scripts/app.js
+++ JavascriptRefresher/assets/scripts/app.js
@@ -1,3 +1,4 @@
+// 1-import & export
 import { Name, pren } from "./utils.js";
 import * as info from "./utils.js"
 import prenom from "./utils.js"
@@ -6,3 +7,10 @@ console.log(pren);
 console.log(info.Name);
 console.log(info.pren);
 console.log(prenom);
+// 2-Revisiting Variables & Values
+let nom = "amine";
+nom = "amine mebarki";
+console.log(nom);
+const adress = "ALGER";
+// a constant does not change, it is just read
+console.log(adress);
