768486afa11cd537efdaeb2d15c9a7fc83040b96
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-10-13 13:34:03 +0100

    Revisiting Objects & Classes

--- JavascriptRefresher/assets/scripts/app.js
+++ JavascriptRefresher/assets/scripts/app.js
@@ -15,33 +15,57 @@ const adress = "ALGER";
 // a constant does not change, it is just read
 console.log(adress);
 // 3-Revisiting Functions & Parameters
-function showName(){
+function showName() {
     console.log("Amine MEBARKI");
 }
 showName();
 
-function showName(Name){
+function showName(Name) {
     console.log(Name);
 }
 showName("Amine MEBARKI");
 
-function showName(nom , Prenom){
+function showName(nom, Prenom) {
     console.log(nom + Prenom);
 }
-showName("Amine"," MEBARKI");
+showName("Amine", " MEBARKI");
 
-function showName(nom , Prenom = "MEBARKI"){
+function showName(nom, Prenom = "MEBARKI") {
     console.log(nom + Prenom);
 }
 showName("Amine");
 
-function showName(nom , Prenom = "MEBARKI"){
+function showName(nom, Prenom = "MEBARKI") {
     console.log(nom + Prenom);
 }
-showName("Amine"," IKRABEM");
+showName("Amine", " IKRABEM");
 // 4-Arrow Functions
 
 const arrowfunc = (nom) => {
-    return "mon nom est" +  nom;
+    return "mon nom est" + nom;
 }
 console.log(arrowfunc("amine"));
+// 5-Revisiting Objects & Classes
+const user = {
+    nom: "Mebarki",
+    prenom: "Amine",
+    Age: 28,
+    Affiche() {
+    console.log("Je me prénome " + this.nom + " " + this.prenom + " et j'ai " + this.Age);
+    }
+}
+user.Affiche();
+
+class User2 {
+    constructor(nom,prenom){
+        this.nom = nom;
+        this.prenom = prenom ; 
+    }
+    affiche(){
+        console.log(this.nom + " " + this.prenom);
+    }
+}
+ let x1 = new User2("Ahmed","chorfi");
+ x1.affiche();
+ let x2 = new User2("said","khatir");
+ x2.affiche();
