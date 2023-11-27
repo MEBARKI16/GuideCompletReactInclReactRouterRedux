51f7f5921df9174316c3c9909ca912ebeab5cb9b
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-11-27 14:24:34 +0100

    Outputting Content Conditionally

--- CalculatorInvestment/src/App.jsx
+++ CalculatorInvestment/src/App.jsx
@@ -4,12 +4,13 @@ import UserInput from "./Components/UserInput"
 import { useState } from "react";
 function App() {
   const [inputValues, setInputValues] = useState({ initialInvestment: 10000, annualInvestment: 1200, expectedReturn: 6, duration: 10 });
-
+  const inputValid = inputValues.duration > 0;
   return (
     <>
       <Header />
       <UserInput setInputValues={setInputValues} inputValues={inputValues} />
-      <Results inputValues={inputValues} />
+      {!inputValid && <p className="center">Please inter a duration greater than zero.</p>}
+      {inputValid && <Results inputValues={inputValues} />}
     </>
 
   )
