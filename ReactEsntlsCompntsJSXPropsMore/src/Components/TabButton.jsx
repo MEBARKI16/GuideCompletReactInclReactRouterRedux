3e85a8b77f22cc9dfdff1a083ae4014cf669d221
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-11-29 10:30:52 +0100

    initialize project

--- ReactEsntlsCompntsJSXPropsMore/src/Components/TabButton.jsx
+++
@@ -1,7 +0,0 @@
-const TabButton = ({ children, onSelect, isActive }) => {
-
-    return (
-        <li><button className={isActive} onClick={onSelect}>{children}</button></li>
-    );
-}
-export default TabButton;
