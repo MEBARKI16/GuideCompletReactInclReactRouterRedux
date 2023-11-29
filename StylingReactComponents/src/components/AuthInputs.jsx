45b492c24303f10bc077d3fc43420f4b3b4023fc
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-11-29 11:09:00 +0100

    Vanilla CSS Styles Are NOT Scoped To Components!

--- StylingReactComponents/src/components/AuthInputs.jsx
+++ StylingReactComponents/src/components/AuthInputs.jsx
@@ -42,6 +42,7 @@ export default function AuthInputs() {
           />
         </p>
       </div>
+      <p>Test Style</p>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
