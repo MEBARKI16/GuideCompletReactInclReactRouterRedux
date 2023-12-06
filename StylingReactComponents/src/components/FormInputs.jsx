9a3d990c0afd6b2a5b7f0e4233f149aa2ecf6d31
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-12-06 17:15:15 +0100

    Dynamic & Conditional Styling with Tailwind

--- StylingReactComponents/src/components/FormInputs.jsx
+++ StylingReactComponents/src/components/FormInputs.jsx
@@ -1,30 +1,9 @@
-import { styled } from 'styled-components'
 
-const Label = styled.label`
-display: block;
-margin-bottom: 0.5rem;
-font-size: 0.75rem;
-font-weight: 700;
-letter-spacing: 0.1em;
-text-transform: uppercase;
-color: ${props => props.$invalid ? '#f87171' : '#6b7280'};
-`;
-const Input = styled.input`
-width: 100%;
-padding: 0.75rem 1rem;
-line-height: 1.5;
-border: 1px solid;
-border-radius: 0.25rem;
-box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-color : ${props => props.$invalid ? '#ef4444' : '#374151'};
-border-color : ${props => props.$invalid ? '#f73f3f' : 'transparent'};
-background-color: ${props => props.$invalid ? '#fed2d2' : 'white'};
-`
 const FormInputs = ({ label, invalid, onChange, type }) => {
     return (
         <p>
-            <Label $invalid={invalid}>{label}</Label>
-            <Input type={type} onChange={onChange} $invalid={invalid} />
+            <label className={`block mb-2 text-[0.75rem] font-bold uppercase tracking-[0.1em] ${invalid ? 'text-[#f87171]' : 'text-[#6b7280]'}`}>{label}</label>
+            <input type={type} onChange={onChange} className={`shadow w-full py-3 px-4 leading-normal border border-solid rounded-[0.25rem] ${invalid ? 'text-[#ef4444] border-[#f73f3f] bg-[#fed2d2]' : 'text-[#374151] border-transparent bg-white '}`} />
         </p>
     );
 }
