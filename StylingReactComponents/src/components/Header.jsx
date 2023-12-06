f96650d278fcc6870241e70f808335c178b68bde
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-12-06 14:31:01 +0100

    Adding & Using Tailwind CSS In A React Project

--- StylingReactComponents/src/components/Header.jsx
+++ StylingReactComponents/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="flex flex-col items-center content-center mt-8 mb-8 md:mb-16">
       <img className="object-contain mb-8 w-44 h-44" src={logo} alt="A canvas" />
-      <h1 className="md:text-4xl text-2xl font-semibold tracking-wider text-center uppercase m-0 text-[#9a3412] font-['Pacifico']">ReactArt</h1>
+      <h1 className="md:text-4xl text-2xl font-semibold tracking-wider text-center uppercase m-0 text-[#9a3412] font-title">ReactArt</h1>
       <p className='text-center text-[#a39191] m-0'>A community of artists and art-lovers.</p>
     </header>
   );
