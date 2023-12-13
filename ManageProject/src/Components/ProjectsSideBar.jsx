d5359ea35c01b0c0a16529fd4e4c2eacbcb7e3fd
Amine MEBARKI <105977621+MEBARKI16@users.noreply.github.com>
2023-12-13 14:04:39 +0100

    update

---
+++ ManageProject/src/Components/ProjectsSideBar.jsx
@@ -0,0 +1,12 @@
+const ProjectSideBar = () => {
+    return (
+        <aside>
+            <h1>Your Projects</h1>
+            <div>
+                <button>+ Add Project</button>
+            </div>
+            <ul></ul>
+        </aside>
+    );
+}
+export default ProjectSideBar;
